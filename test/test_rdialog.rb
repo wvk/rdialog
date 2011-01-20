c049065683cc107b43eb38e63d6e4c7e0888325f
Willem van Kerkhof <willem.van-kerkhof@innoq.com>
2011-01-20 16:07:34 +0100

    More or less a complete rewrite:  * Get rid of Tempfile usage and use backticks instead.  * Cleanup Indentation and String Concatenation (use string interpolation instead)  * Get rid of many repetitive boilerplate code

--- test/test_rdialog.rb
+++ test/test_rdialog.rb
@@ -3,9 +3,17 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 class TestRdialog < Test::Unit::TestCase
 
   def setup
+    @dialog = RDialog.new
+    @dialog.title = 'foo'
   end
-  
-  def test_truth
-    assert true
+
+  def test_yesno
+    assert @dialog.yesno('please answer YES'), 'did you really answer YES?!'
+    assert !@dialog.yesno('please answer NO'), 'did you really answer NO?!'
   end
+
+  def test_textbox
+    assert @dialog.textbox('test/test_helper.rb')
+  end
+
 end
